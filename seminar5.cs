f953de26b55e0967e580c4568e43c29f5e5afce9
NamikR <uchitelnamik@mail.ru>
2022-10-15 16:47:48 +0400

    Задача 36 завершена

--- seminar5.cs
+++ seminar5.cs
@@ -1,21 +1,82 @@
-﻿// Задача 34: 
-// Задайте массив заполненный случайными положительными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
-// [345, 897, 568, 234] -> 2
+﻿// // Задача 34: 
+// // Задайте массив заполненный случайными положительными трёхзначными числами. 
+// // Напишите программу, которая покажет количество чётных чисел в массиве.
+// // [345, 897, 568, 234] -> 2
 
-// Задача 34: ---Начало--------------------------------------------------------------
+// // Задача 34: ---Начало--------------------------------------------------------------
 
-Console.WriteLine("Задача 34\n Задайте массив заполненный случайными положительными трёхзначными числами.\n Напишите программу, которая покажет количество чётных чисел в массиве.");
+// Console.WriteLine("Задача 34\n Задайте массив заполненный случайными положительными трёхзначными числами.\n Напишите программу, которая покажет количество чётных чисел в массиве.");
+// Console.WriteLine();
+// Console.WriteLine("Решение");   
+// Console.WriteLine();
+
+// int [] CreatMyArray  (int size, int minValue, int maxValue)
+// {
+//     int[] myArray = new int [size];
+//     for (int i =0; i < size; i++)
+//     {
+//       myArray[i] = new Random().Next(minValue, maxValue);
+
+//     }
+
+//     return myArray;
+
+// }
+
+// int amountOfEvenNumbersInArray=0;
+
+// void ShowArray(int[] myShowArray)
+// {
+
+//      for(int i=0; i < myShowArray.Length; i++)
+//     {
+//        Console.Write(myShowArray[i] + " ");
+
+//        if (myShowArray[i] % 2 == 0)
+//        {       
+
+//           if (myShowArray[i] % 2 == 0)
+//             {
+//                amountOfEvenNumbersInArray++;                        
+          
+//             }                
+          
+//        }       
+
+//     }
+  
+//     Console.WriteLine();
+//     Console.WriteLine("Количество чётных чисел в массиве " + amountOfEvenNumbersInArray); 
+// }
+
+// Console.Write("Введите размер массива ");
+// int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+// int [] fillingArray = CreatMyArray(sizeArray, 100, 999);
+
+// ShowArray(fillingArray);
+
+// // Задача 34: ---Конец--------------------------------------------------------------
+
+// // Задача 36: ---Начало--------------------------------------------------------------
+
+// Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Задача 36\n Задайте одномерный массив, заполненный случайными числами..\n Найдите сумму элементов, стоящих на нечётных позициях..");
 Console.WriteLine();
 Console.WriteLine("Решение");   
 Console.WriteLine();
 
-int [] CreatMyArray  (int size, int minValue, int maxValue)
+int [] CreatMyArray  (int size)
 {
     int[] myArray = new int [size];
     for (int i =0; i < size; i++)
     {
-      myArray[i] = new Random().Next(minValue, maxValue);
+      myArray[i] = new Random().Next(0, size);
 
     }
 
@@ -23,7 +84,7 @@ int [] CreatMyArray  (int size, int minValue, int maxValue)
 
 }
 
-int amountOfEvenNumbersInArray=0;
+int sumOfElementsInOddPositions=0;
 
 void ShowArray(int[] myShowArray)
 {
@@ -35,9 +96,9 @@ void ShowArray(int[] myShowArray)
        if (myShowArray[i] % 2 == 0)
        {       
 
-          if (myShowArray[i] % 2 == 0)
+          if (i % 2 == 1)
             {
-               amountOfEvenNumbersInArray++;                        
+               sumOfElementsInOddPositions += myShowArray[i];                        
           
             }                
           
@@ -46,14 +107,14 @@ void ShowArray(int[] myShowArray)
     }
   
     Console.WriteLine();
-    Console.WriteLine("Количество чётных чисел в массиве " + amountOfEvenNumbersInArray); 
+    Console.WriteLine("Сумма элементов, стоящих на нечётных позициях в массиве равна " + sumOfElementsInOddPositions); 
 }
 
 Console.Write("Введите размер массива ");
 int sizeArray = Convert.ToInt32(Console.ReadLine());
 
-int [] fillingArray = CreatMyArray(sizeArray, 100, 999);
+int [] fillingArray = CreatMyArray(sizeArray);
 
 ShowArray(fillingArray);
 
-// Задача 34: ---Конец--------------------------------------------------------------
+// // Задача 36: ---Конец--------------------------------------------------------------
